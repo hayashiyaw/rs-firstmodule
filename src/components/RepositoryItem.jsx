0e542e48e0cbe9e621bf2e8533e38213109ed937
hayashiyaw <whayashiyaw@gmail.com>
2021-11-12 17:57:41 -0300

    first commit

---
+++ src/components/RepositoryItem.jsx
@@ -0,0 +1,9 @@
+export function RepositoryItem() {
+  return (
+    <li>
+      <strong> {repositoryName} </strong>
+      <p>Forms in React</p>
+      <a href=""> Acessando diretório</a>
+    </li>
+  )
+}
